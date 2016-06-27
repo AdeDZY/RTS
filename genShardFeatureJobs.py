672ca28b5b983f9909b5a2715e8785b660e25fac
zhuyund <zhuyund@cs.cmu.edu>
2016-06-26 22:47:16 -0400

    getShardFeatureJobs.py

--- genShardFeatureJobs.py
+++ genShardFeatureJobs.py
@@ -5,80 +5,37 @@ __author__ = 'zhuyund'
 # for a partition
 # gen jobs for shardFeature
 
-
-import argparse
 import os, sys
 import jobWriter
 from os import listdir
 from os.path import isfile, join
 
-parser = argparse.ArgumentParser()
-parser.add_argument("partition_name")
-parser.add_argument("shardmaps_dir")
-parser.add_argument("repo_dir")
-parser.add_argument("query_term_file")
-parser.add_argument("--bigram", "-b", action="store_true")
-parser.add_argument("--fielded", action="store_true")
-parser.add_argument("--nospam", "-s", action="store_true")
-parser.add_argument("--spamfile", "-f", type=str, default="/bos/usr0/zhuyund/fedsearch/cw09b.spam")
-args = parser.parse_args()
+if __name__ == '__main__':
 
-base_dir = "/bos/usr0/zhuyund/partition/ShardFeature/output/" + args.partition_name
-print base_dir
+    dirs = []
+    out_dirs = []
+    for y in range(12, 16):
+        dirs += ["output/text_{0}{1:02d}s".format(y, i) for i in range(1, 13)]
+        out_dirs += ["output/vectors_{0}{1:02d}s".format(y, i) for i in range(1, 13)]
+    dirs += ["output/text_160{0}s".format(i) for i in range(1, 6)]
+    out_dirs += ["output/vectors_160{0}s".format(i) for i in range(1, 6)]
 
+    job_file = open("get_vectors.job")
 
-if not os.path.exists(base_dir):
-    os.makedirs(base_dir)
+    for d, od in zip(dirs, out_dirs):
+        execuatable = "./get_vectors.py"
+        arguments = "{0} {1} {2}".format(d,
+                                         "vocab_4year",
+                                         od)
 
-jobs_dir = base_dir + '/jobs'
-if not os.path.exists(jobs_dir):
-    os.makedirs(jobs_dir)
+        log = "/tmp/zhuyund_kmeans.log"
+        out = "tmp/vectors.out"
+        err = "tmp/vectors.err"
 
-feat_dir = base_dir + '/features'
-if not os.path.exists(feat_dir):
-    os.makedirs(feat_dir)
+        job = jobWriter.jobGenerator(execuatable, arguments, log, err, out)
+        job_file.write(job + '\n')
 
-shardmaps = [f.strip() for f in listdir(args.shardmaps_dir)
-             if isfile(join(args.shardmaps_dir, f)) and f.isdigit()]
-
-shardmap_file = open(base_dir + "/shards", 'w')
-for f in shardmaps:
-    shardmap_file.write(f + '\n')
-shardmap_file.close()
-
-for shardmap in shardmaps:
-    execuatable = "./shardFeature"
-    arguments = "{0} {1} {2} {3}".format(args.repo_dir,
-                                         args.shardmaps_dir + '/' + shardmap,
-                                         feat_dir + '/' + shardmap + '.feat',
-                                         args.query_term_file)
-    if args.nospam:
-        execuatable = "./shardFeature_nospam"
-        arguments = "{0} {1} {2} {3} {4}".format(args.repo_dir,
-                                         args.shardmaps_dir + '/' + shardmap,
-                                         feat_dir + '/' + shardmap + '.feat',
-                                         args.query_term_file,
-                                         args.spamfile)
-    if args.bigram:
-        execuatable = "./shardFeature_bigram"
-        arguments = "{0} {1} {2} {3}".format(args.repo_dir,
-                                         args.shardmaps_dir + '/' + shardmap,
-                                         feat_dir + '/' + shardmap + '.feat_bigram',
-                                         args.query_term_file)
-    if args.fielded:
-        execuatable = "./shardFeature_fielded"
-        arguments = "{0} {1} {2} {3}".format(args.repo_dir,
-                                         args.shardmaps_dir + '/' + shardmap,
-                                         feat_dir + '/' + shardmap + '.feat_fielded',
-                                         args.query_term_file)
-    log = "/tmp/zhuyund_kmeans.log"
-    out = base_dir + "/out"
-    err = base_dir + "/err"
-
-    job = jobWriter.jobGenerator(execuatable, arguments, log, err, out)
-    job_file = open("{0}/{1}_feat.job".format(jobs_dir, shardmap), 'w')
-    job_file.write(job)
-    job_file.close()
+job_file.close()
 
 
 
