4d038141b8b72f2062140fce48fd5556e2dd58a7
zhuyund <zhuyund@cs.cmu.edu>
2016-06-27 14:32:21 -0400

    gen_avghashvec_job

---
+++ gen_avghashvec_job.py
@@ -0,0 +1,42 @@
+#!/opt/python27/bin/python
+
+__author__ = 'zhuyund'
+
+# for a partition
+# gen jobs for shardFeature
+
+import os, sys
+import jobWriter
+from os import listdir
+from os.path import isfile, join
+
+if __name__ == '__main__':
+
+    dirs = []
+    out_dirs = []
+    vec_dirs = []
+    for y in range(12, 16):
+        vec_dirs += ["output/vectors_{0}{1:02d}s".format(y, i) for i in range(1, 13)]
+        out_dirs += ["output/avgvec/{0}{1:02d}s".format(y, i) for i in range(1, 13)]
+        dirs += ["data/{0}{1:02d}s_extid".format(y, i) for i in range(1, 13)]
+    vec_dirs += ["output/vectors_160{0}s".format(i) for i in range(1, 6)]
+    out_dirs += ["output/avgvec/160{0}s".format(i) for i in range(1, 6)]
+    dirs += ["data/160{0}s_extid".format(i) for i in range(1, 6)]
+
+    job_file = open("get_avghashvec.job", 'w')
+
+    for i in range(len(dirs)):
+        execuatable = "./get_avg_hashvec.py"
+        arguments = "{0} {1} {2}".format(dirs[i], vec_dirs[i], out_dirs[i])
+
+        log = "/tmp/zhuyund_kmeans.log"
+        out = "tmp/vectors.out"
+        err = "tmp/vectors.err"
+
+        job = jobWriter.jobGenerator(execuatable, arguments, log, err, out)
+        job_file.write(job + '\n')
+
+job_file.close()
+
+
+
